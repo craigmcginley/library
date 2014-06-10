1958535760178fd99e41fbf902169b5cf2c82b7b
craigmcginley <craigmcginley@gmail.com>
2014-06-10 11:30:49 -0400

    added readers table

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ActiveRecord::Base
   validates :name, presence: true
 
   has_many :categorizations
-  has_many :books, through :categorizations
+  has_many :books, through: :categorizations
 end
